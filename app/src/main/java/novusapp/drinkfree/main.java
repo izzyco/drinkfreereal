5c8e9d61012ddcc5767f6d933e66bcfcd4cacf6a
izhang1 <ivanzhangemail@gmail.com>
2016-02-06 13:09:46 -0500

    returned typeface back to normal

--- app/src/main/java/novusapp/drinkfree/main.java
+++ app/src/main/java/novusapp/drinkfree/main.java
@@ -14,6 +14,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
